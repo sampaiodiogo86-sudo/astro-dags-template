2bb0ba965c8d42258fa83bbd0d33fb6cd47dec88
sampaiodiogo86-sudo <sampaiodiogo86@gmail.com>
2025-09-24 22:01:48 -0300

    Update bitcoin_etl_bigquery.py

--- dags/bitcoin_etl_bigquery.py
+++ dags/bitcoin_etl_bigquery.py
@@ -106,6 +106,7 @@ def fetch_and_to_gbq():
     schedule="0 0 * * *",  # daily at 00:00 UTC
     start_date=pendulum.datetime(2025, 9, 17, tz="UTC"),
     catchup=True,
+    max_active_runs=1,
     owner_links={
         "Alex Lopes": "mailto:alexlopespereira@gmail.com",
         "Open in Cloud IDE": "https://cloud.astronomer.io/cm3webulw15k701npm2uhu77t/cloud-ide/cm42rbvn10lqk01nlco70l0b8/cm44gkosq0tof01mxajutk86g",
